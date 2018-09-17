ec33cc44edb0b6cbcd49a31e261c8c47f7437ea1
kitri <kitri@DESKTOP-9593NSN>
2018-09-17 17:24:43 +0900

    tt

--- src/main/java/erp/pass/controller/PassController.java
+++ src/main/java/erp/pass/controller/PassController.java
@@ -9,13 +9,6 @@ public class PassController {
 	public String insertView(Model model){
 		String today = new java.text.SimpleDateFormat ("yyyy-MM-dd").format(new java.util.Date()); 
 		//System.out.println(today);
-		/*GregorianCalendar today = new GregorianCalendar();
-		 
-		int Year = today.get(Calendar.YEAR);
-		int Month = today.get(Calendar.MONTH);
-		int Date = today.get(Calendar.DATE);
-		int Hour = today.get(Calendar.HOUR_OF_DAY);
-		 */
 		model.addAttribute("today", today);
 		return "erp/pass";
 	}
