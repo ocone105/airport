2491bc923ae58f0148a2a8c5e80873b39e7bf109
kitri <kitri@DESKTOP-9NEDNG8>
2018-10-02 16:30:03 +0900

    ...

--- src/main/java/main/realtime/controller/RealtimeController.java
+++ src/main/java/main/realtime/controller/RealtimeController.java
@@ -1,16 +1,7 @@
 package main.realtime.controller;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
 import java.util.ArrayList;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
