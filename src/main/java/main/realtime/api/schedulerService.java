cc98d6670d4188ccf29d9512b08bb7d5de57439f
Ocean <Ocean@DESKTOP-TAFB420>
2018-10-04 01:53:04 +0900

    181004 비정상운항완료

--- src/main/java/main/realtime/api/schedulerService.java
+++ src/main/java/main/realtime/api/schedulerService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class schedulerService {
 
-	@Scheduled(cron="* * 1 * * ?")	// 매일 오전 1시에 실행
+	@Scheduled(cron="* * 10 * * ?")	// 매일 오전 1시에 실행
 	public void getWeather() {
 		System.out.println("스케쥴링 테스트");
 	}
